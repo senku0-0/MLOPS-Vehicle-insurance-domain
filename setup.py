286c0570df6224102a6ff768d41ae96bd63140ec
senku0-0 <ashadshaikh.zx@gmail.com>
2025-10-07 16:48:05 +0530

    learnt -e . does and why i need .toml and setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ashad Shaikh",
+    author_email="ashadshaikh.zx@gmail.com",
+    packages=find_packages()
+)
