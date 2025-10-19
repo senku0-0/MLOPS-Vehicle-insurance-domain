a484cd9c02ce6be68c83bcf60833bc05b85b8627
senku0-0 <ashadshaikh.zx@gmail.com>
2025-10-19 11:17:41 +0530

    installed dotenv and loaded .env in mongo_db_connection.py

--- src/configuration/mongo_db_connection.py
+++ src/configuration/mongo_db_connection.py
@@ -2,10 +2,12 @@ import os
 import sys
 import pymongo
 import certifi
-
+from dotenv import load_dotenv
 from src.exception import MyException
 from src.logger import logging
 from src.constants import DATABASE_NAME, MONGODB_URL_KEY
+
+load_dotenv()
 
 # Load the certificate authority file to avoid timeout errors when connecting to MongoDB
 ca = certifi.where()
